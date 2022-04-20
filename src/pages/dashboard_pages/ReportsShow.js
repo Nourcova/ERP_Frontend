ebe5ec2e1a8ee1bb5ef0c788bb4cd1cda672260b
Nourcova <ibrahamnour22@gmail.com>
2022-04-20 14:42:21 +0300

    fix image problem

--- src/pages/dashboard_pages/ReportsShow.js
+++ src/pages/dashboard_pages/ReportsShow.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import emp from '../../assets/img/emp.jpg'
 import { Outlet } from "react-router-dom";
 import { NavLink } from 'react-router-dom';
@@ -16,7 +16,7 @@ function ReportsShow() {
     let res = await axios.get(`https://sleepy-wave-82877.herokuapp.com/api/employees/${id}`)
     try {
       setEmployee(res.data.data)
-    }catch(err){
+    } catch (err) {
       console.log(err)
     }
   }
@@ -26,29 +26,29 @@ function ReportsShow() {
     AOS.init();
     AOS.refresh();
     getEmployee()
-    }, []);
+  }, []);
 
   return (
     <div className='report-show'>
       <div className='nav-reports' data-aos-duration="1500" data-aos="fade-down">
         <div className='first' data-aos-duration="2500" data-aos="fade-right">
-          <img src={emp} alt='' />
+          <img src={`https://sleepy-wave-82877.herokuapp.com/image/${employee.image}`}/>
           <h5>{employee.first_name + ' ' + employee.last_name}</h5>
         </div>
         <div className='type' data-aos-duration="2500" data-aos="fade-left">
 
-            <NavLink  className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportData/kpis`}>
-              KPIS
-            </NavLink>
-          
-            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportData/projects`}>
-              Projects
-            </NavLink>
-  
-            <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportGraph`}>
-             Graph
-            </NavLink>
-        
+          <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportData/kpis`}>
+            KPIS
+          </NavLink>
+
+          <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportData/projects`}>
+            Projects
+          </NavLink>
+
+          <NavLink className={({ isActive }) => (isActive ? 'activeLink' : 'inactive')} to={`/dashboard/reports/${id}/reportGraph`}>
+            Graph
+          </NavLink>
+
         </div>
       </div>
       <div className='nav-details'>
